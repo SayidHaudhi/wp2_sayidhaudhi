82bd436e26659e69512bdc4e8c1f393b99b1c848
SayidHaudhi <sayidalbatawie@gmail.com>
2024-05-30 11:45:06 +0700

    Pertemuan 9

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Raifelnavies
+ Saya tingga di daerah Jaksel
+ olah raga yang saya sukai adalah
+ Bus";
+ }
+}
